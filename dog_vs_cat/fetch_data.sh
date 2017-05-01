3d69b2f96e9464457b07c343d8eccdabedacf6ff
东武 <zhiliang.xzl@alibaba-inc.com>
2017-05-01 10:45:55 +0800

    fetch data

---
+++ dog_vs_cat/fetch_data.sh
@@ -0,0 +1,6 @@
+wget https://www.kaggle.com/c/dogs-vs-cats-redux-kernels-edition/download/train.zip
+wget https://www.kaggle.com/c/dogs-vs-cats-redux-kernels-edition/download/test.zip
+
+mkdir -p data train test log 
+
+
