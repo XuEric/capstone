41ad93dd8568399a83a8bedea56c19a31151e7df
东武 <zhiliang.xzl@alibaba-inc.com>
2017-05-01 15:11:34 +0800

    dummy

--- dog_vs_cat/fetch_data.sh
+++ dog_vs_cat/fetch_data.sh
@@ -1,5 +1,7 @@
-wget https://www.kaggle.com/c/dogs-vs-cats-redux-kernels-edition/download/train.zip
-wget https://www.kaggle.com/c/dogs-vs-cats-redux-kernels-edition/download/test.zip
+
+wget "https://kaggle2.blob.core.windows.net/competitions-data/kaggle/5441/train.zip?sv=2015-12-11&sr=b&sig=htsrJNpdda%2BJVe90l0IBIxmpg3ygbpn3ariNs1E7wMs%3D&se=2017-05-04T02%3A52%3A37Z&sp=r"
+
+wget"https://kaggle2.blob.core.windows.net/competitions-data/kaggle/5441/test.zip?sv=2015-12-11&sr=b&sig=WrktimWagLAdLmOehtU0AIN2HopK1kvGnTPK0qr9YOw%3D&se=2017-05-04T02%3A53%3A46Z&sp=r"
 
 mkdir -p data train test log 
 
